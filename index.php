cd56752416c949372c4cf34239b03129a141ca41
Kushal Samota <kushalsamota@gmail.com>
2022-05-27 10:16:12 +0530

    fd

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
 <?php
-echo "\n Good Morning, I am running on openshift version4.6 ".gethostname(); 
+echo "\n Good Afternoon, I am running on openshift version4.6 ".gethostname(); 
 echo "\n\n";
 ?>
