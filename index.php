b4a8a148cb947b3b187c059d290cf21cd22f83c0
Kushal Samota <kushalsamota@gmail.com>
2024-03-20 12:25:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
 <?php
-echo "\n Happy Morning, How are you? I am running on openshift version4.10 ".gethostname(); 
+echo "\n Happy Morning, How are you? I am running on openshift version4.14 ".gethostname(); 
 echo "\n\n";
 ?>
